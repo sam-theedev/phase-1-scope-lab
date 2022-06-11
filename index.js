4688d49408045c9d8a27f2405098ca65acd4de7c
Samson <samblint@gmail.com>
2022-06-11 16:28:23 +0300

    commit tree 59d63624fdbd7b5db193c9b31681ba956d37072c

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'sam'
+function customerName(){
+    return customerName();
+
+}
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+  
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+  }
+  
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'billy'
+
+  }
+  function changeLeastFavoriteCustomer() {
+     leastFavoriteCustomer = 'alex'
+  }
